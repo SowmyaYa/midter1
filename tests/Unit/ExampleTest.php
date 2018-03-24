6d0dd78984ae630242d71e0d98ad079456f45dfd
sowmya <sy379@njit.edu>
2018-03-23 22:39:19 -0400

    Feature test to check register page returning status code 200. This is the ninth commit for this project.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
      * A basic test example.
      *
      * @return void
-     */
+    */
     public function testBasicTest()
     {
         $this->assertTrue(true);
