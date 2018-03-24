e77976ec1e8191706b072c757005c1a6da5977a0
sowmya <sy379@njit.edu>
2018-03-24 14:55:01 -0400

    Created a unit test to update a record in users  table. This is my 16th commit for this project.

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -13,29 +13,42 @@ class UserTest extends TestCase
      * A basic test example.
      *
      * @return void
-
-    public function testOne()
+    */
+    /*public function testOne()
     {
         $user = new User();
         $user->name = 'Harry';
         $user->email = 'harry@potter.com';
         $user->password = 'harrypotter';
         $this->assertTrue($user->save());
-    }
-     */
+    }*/
+
+    //Test  for deletion
     public function testTwo()
     {
         $user = new User();
-        $user->name = 'Harry';
+        $user->name = 'Steve Smith';
         $user->email = 'harry@potter.com';
         $user->password = 'harrypotter';
         $user->save();
         $this->assertTrue($user->delete());
     }
+    //Test for counting
     public function testThree(){
         $users=User::All();
         $userCount = $users->count();
         $actual=50;
         $this->assertEquals($userCount,$actual);
     }
+    //Test for updation
+    public function testFour()
+    {
+        $user = new User();
+        $user->name = 'Steve Smith';
+        $user->email = 'harry@potter.com';
+        $user->password = 'harrypotter';
+        $this->assertTrue($user->save());
+    }
+
+
 }
